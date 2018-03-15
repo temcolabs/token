48cfa6453c10e5798d4def9b7c9eb3acfac356ac
temcolabs <37317523+temcolabs@users.noreply.github.com>
2018-03-15 01:29:42 -0400

    updated version

--- TemcoToken.sol
+++ TemcoToken.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.4.18;
 import "./Ownable.sol";
 import "./ERC20.sol";
 import "./SafeMath.sol";
+import "./Lockable.sol";
 
 
 /**
  * @title Temco token
- * Based on code by https://https://github.com/OpenZeppelin/zeppelin-solidity
+ * @dev Based on code by https://https://github.com/OpenZeppelin/zeppelin-solidity
+ * @author Geunil(Brian) Lee
  */
-contract TemcoToken is ERC20, Ownable {
+contract TemcoToken is ERC20, Ownable, Lockable {
   
   using SafeMath for uint256;
   
@@ -56,7 +58,7 @@ contract TemcoToken is ERC20, Ownable {
   * @param _to The address to transfer to.
   * @param _value The amount to be transferred.
   */
-  function transfer(address _to, uint256 _value) public onlyOwner returns (bool) {
+  function transfer(address _to, uint256 _value) public whenNotLockedUp returns (bool) {
     emit Transfer(msg.sender, _to, _value);
     emit OwnedValue(msg.sender, _value);
     require(_to != address(0));
@@ -75,7 +77,7 @@ contract TemcoToken is ERC20, Ownable {
    * @param _to address The address which you want to transfer to
    * @param _value uint256 the amount of tokens to be transferred
    */
-  function transferFrom(address _from, address _to, uint256 _value) public  onlyOwner returns (bool) {
+  function transferFrom(address _from, address _to, uint256 _value) public whenNotLockedUp returns (bool) {
     require(_to != address(0));
     require(_value <= balances[_from]);
     require(_value <= allowed[_from][msg.sender]);
@@ -91,14 +93,16 @@ contract TemcoToken is ERC20, Ownable {
    * @dev Transfer tokens from one address to another without approval
    * @param _from address The address which you want to send tokens from
    * @param _to address The address which you want to transfer to
-   * @param _value uint256 the amount of tokens to be transferred
+   * @param _value the amount of tokens to be transferred
+   * @param _lockUpDuriation lock up duration for the address
    */
-  function transferFromWithoutApproval(address _from, address _to, uint256 _value) public  onlyOwner returns (bool) {
+  function transferFromWithoutApproval(address _from, address _to, uint256 _value, uint256 _lockUpDuriation) public  onlyOwner whenNotLockedUp returns (bool) {
     require(_to != address(0));
     require(_value <= balances[_from]);
     
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
+    addLockUp(_to, _lockUpDuriation);
     emit Transfer(_from, _to, _value);
     return true;
   }
@@ -113,7 +117,7 @@ contract TemcoToken is ERC20, Ownable {
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
-  function approve(address _spender, uint256 _value) public onlyOwner returns (bool) {
+  function approve(address _spender, uint256 _value) public whenNotLockedUp returns (bool) {
     allowed[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
     return true;
@@ -148,7 +152,7 @@ contract TemcoToken is ERC20, Ownable {
    * @param _spender The address which will spend the funds.
    * @param _addedValue The amount of tokens to increase the allowance by.
    */
-  function increaseApproval(address _spender, uint _addedValue) public onlyOwner returns (bool) {
+  function increaseApproval(address _spender, uint _addedValue) whenNotLockedUp public returns (bool) {
     allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
     emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
     return true;
@@ -164,7 +168,7 @@ contract TemcoToken is ERC20, Ownable {
    * @param _spender The address which will spend the funds.
    * @param _subtractedValue The amount of tokens to decrease the allowance by.
    */
-  function decreaseApproval(address _spender, uint _subtractedValue) public onlyOwner returns (bool) {
+  function decreaseApproval(address _spender, uint _subtractedValue) whenNotLockedUp public returns (bool) {
     uint oldValue = allowed[msg.sender][_spender];
     if (_subtractedValue > oldValue) {
       allowed[msg.sender][_spender] = 0;
@@ -203,6 +207,7 @@ contract TemcoToken is ERC20, Ownable {
    * @return A boolean that indicates if the operation was successful.
    */
   function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+    require(_to != address(0) && _amount > 0);
     totalSupply = totalSupply.add(_amount);
     balances[_to] = balances[_to].add(_amount);
     emit Mint(_to, _amount);
@@ -219,6 +224,5 @@ contract TemcoToken is ERC20, Ownable {
     emit MintFinished();
     return true;
   }
-  
   
 }
