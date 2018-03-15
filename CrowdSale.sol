1a83a64a884546bf19a86b795d75b7aa3b4726a1
temcolabs <37317523+temcolabs@users.noreply.github.com>
2018-03-15 01:27:54 -0400

    updated version

--- CrowdSale.sol
+++ CrowdSale.sol
@@ -5,7 +5,7 @@ import "./TemcoToken.sol";
 import "./SafeMath.sol";
 
 /**
- * @title Temco token crowdsale
+ * @title TEMCO token crowdsale
  * @dev To run crowdsale. This contract communicates with Temco token contract.
  * @author Geunil(Brian) Lee
  */
@@ -13,15 +13,15 @@ contract CrowdSale is Ownable{
     
     using SafeMath for uint256;
     
-    event TransferCoinToInvester(address invester, uint256 value);
-    event Received(address invester, uint256 value);
+    event TransferCoinToInvestor(address investor, uint256 value);
+    event Received(address investor, uint256 value);
     
     TemcoToken temcoTokenContract;
     address public temcoTokenAddress;
     address public temcoEtherAddress;
     
     /**
-     * Amount raised on crowdsale
+     * @dev Amount raised on crowdsale
      */
     uint public amountRaised;
     function getAmountRaised() public constant returns (uint) {
@@ -29,40 +29,44 @@ contract CrowdSale is Ownable{
     }
     
     /**
-     * Crowd sale start time
+     * @dev Crowd sale start time
      */
     uint public crowdStartTime;
     
     /**
-     * Crowd sale phase dealine. Sale will have total 3 phases.
+     * @dev Crowd sale phase dealine. Sale will have total 3 phases.
      */
     uint public crowdEndTime;
     
     /**
-     * Mininum ether acceptance for invest
+     * @dev Mininum ether acceptance for invest
      */
     uint public minimumEther;
     
     /**
-     * Phase ether goal
+     * @dev Phase ether goal
      */
     uint public goal;
     
     /**
-     * Phase ether to temco token conversion rate
+     * @dev Phase ether to temco token conversion rate
      */
     uint public conversionRate;
     
+    /**
+     * @dev Lock up duration for current sale
+     */
+    uint public lockUpDuration;
     
     /**
-     * Hold who and how much invest on phase
+     * @dev Hold who and how much invest on phase
      */
     mapping (address => uint256) public balances;
     function getBalance(address index) public constant returns (uint256) {
         return balances[index];
     }
     /**
-     * No iteration for the mapping. Hold invester address to use iterate over maping
+     * @dev No iteration for the mapping. Hold investor address to use iterate over maping
      */
     address[] public balanceList;
     function getBalanceList() public constant returns (address[]) {
@@ -70,7 +74,7 @@ contract CrowdSale is Ownable{
     }
 
     /**
-     * KYC block list
+     * @dev KYC block list
      */
     mapping (address => bool) public kycBlockedMap;
     address[] public kycBlockedMapList;
@@ -84,11 +88,12 @@ contract CrowdSale is Ownable{
     * @param temcoAddress temco token address
     * @param temcoTokenContractAddress temco token contract address
     * @param etherAddress wallet to transfer amount raised
-    * @param crowdStartInMinutes crowd sale start time
-    * @param crowdDurationInMinutes crowd sale duration
-    * @param minEther minimum ether to receive
+    * @param crowdStartInMinutes crowd sale start time. unit is days
+    * @param crowdDurationInMinutes crowd sale duration. unit is days
+    * @param minEther minimum ether to receive. unit is 0.1 ether
     * @param fundingGoal funding goal for crowd sale
-    * @param rate conversion rate from ether to temco coi
+    * @param rate conversion rate from ether to temco coin
+    * @param lockUp lock up duration for current sale. unit is days
     */
     function CrowdSale(
         address temcoAddress,
@@ -98,25 +103,32 @@ contract CrowdSale is Ownable{
         uint crowdDurationInMinutes,
         uint minEther,
         uint fundingGoal,
-        uint rate
+        uint rate,
+        uint lockUp
     ) public {
         temcoTokenAddress = temcoAddress;
         temcoTokenContract = TemcoToken(temcoTokenContractAddress);
         temcoEtherAddress = etherAddress;
         //TODO: change to date
+        //crowdStartTime = now + crowdStartInMinutes * 1 days;
+        //crowdEndTime = crowdStartTime + crowdDurationInMinutes * 1 days;
         crowdStartTime = now + crowdStartInMinutes * 1 minutes;
         crowdEndTime = crowdStartTime + crowdDurationInMinutes * 1 minutes;
+        
         minimumEther = minEther * 0.1 ether;
         goal = fundingGoal;
         conversionRate = rate;
+        //TODO: change to date
+        //lockUpDuration = now + lockUp * 1 days;
+        lockUpDuration = now + lockUp * 1 minutes;
     }
     
     /**
    * @dev Reverts if not in crowdsale time range. 
    */
    modifier onlyWhileOpen {
-    require((now >= crowdStartTime && now < crowdEndTime) || (amountRaised < goal));
-    _;
+       require(isClowdsaleOpen() == true);
+       _;
    }
    
    /**
@@ -141,6 +153,7 @@ contract CrowdSale is Ownable{
     * The function without name is the default function that is called whenever anyone sends funds to a contract
     */
     function () payable public onlyWhileOpen minimumEtherRequired{
+        require(msg.sender != address(0));
         uint amount = msg.value;
         
         amountRaised = amountRaised.add(amount);
@@ -151,8 +164,19 @@ contract CrowdSale is Ownable{
 		
     }
     
+    function isClowdsaleOpen() private returns(bool){
+        bool open = false;
+        if(amountRaised < goal){
+           if(now >= crowdStartTime && now < crowdEndTime){
+               open =  true;
+           }
+        }
+        return open;
+    }
+    
     /**
-     * Add kyc block address
+     * @dev Add kyc block address
+     * @param blockAddress address to be added to block list
      */
     function addBockList(address blockAddress) public onlyOwner {
         require(blockAddress != address(0));
@@ -161,7 +185,8 @@ contract CrowdSale is Ownable{
     }
     
     /**
-     * Remove kyc block address
+     * @dev Remove kyc block address
+     * @param blockAddress address to be removed from block list
      */
     function removeBockList(address blockAddress) public onlyOwner {
         require(blockAddress != address(0));
@@ -174,22 +199,22 @@ contract CrowdSale is Ownable{
     }
     
     /**
-     * Send token to investers
+     * @dev Send token to investors
      */
     function distributeCoin() public crowdSaleClosed onlyOwner{
         for (uint index = 0; index < balanceList.length ; index++){
             if(kycBlockedMap[balanceList[index]] != true){
                 require(balances[balanceList[index]] > 0);
-                temcoTokenContract.transferFromWithoutApproval(temcoTokenAddress, balanceList[index], balances[balanceList[index]].mul(conversionRate));
+                temcoTokenContract.transferFromWithoutApproval(temcoTokenAddress, balanceList[index], balances[balanceList[index]].mul(conversionRate), lockUpDuration);
                 balances[balanceList[index]] = balances[balanceList[index]].sub(balances[balanceList[index]]);
                 
-                emit TransferCoinToInvester(temcoTokenAddress, balances[balanceList[index]].mul(conversionRate));
+                emit TransferCoinToInvestor(temcoTokenAddress, balances[balanceList[index]].mul(conversionRate));
             }
         }
     }
     
     /**
-     * Send amount raised ether to wallet
+     * @dev Send amount raised ether to wallet
      */
     function withdrawal() public crowdSaleClosed onlyOwner{
         require(amountRaised > 0);
@@ -197,16 +222,20 @@ contract CrowdSale is Ownable{
     }
     
     /**
-     * In case of goal is changed after contract deployed.
+     * @dev In case of goal is changed after contract deployed.
+     * @param amount sale goal. unit is 0.1 ether
      */
     function updateGoal(uint amount) public onlyOwner{
+        require(amount > 0);
         goal = amount * 0.1 ether;
     }
     
     /**
-     * In case of conversion is changed after contract deployed.
+     * @dev In case of conversion is changed after contract deployed.
+     * @param rate conversion rate
      */
     function updateConversionRate(uint rate) public onlyOwner{
+        require(rate > 0);
         conversionRate = rate;
     }
     
